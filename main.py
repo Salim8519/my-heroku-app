3ffe910a96f3f8fa2d54fcdffd6bf2a914f99d70
Salim Alsenani <mcte.engineer.s@gmail.com>
2025-06-03 16:23:42 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Fly.io!"
+    return "Hello f rom Fly.io!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))  # Use port from Fly.io
